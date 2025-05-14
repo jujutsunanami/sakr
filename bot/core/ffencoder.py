b8d97bfbc66f4ae37a500549b5b0c02a42856a47
jujutsunanami <87896528+jujutsunanami@users.noreply.github.com>
2025-05-14 18:22:42 +0530

    Update ffencoder.py

--- bot/core/ffencoder.py
+++ bot/core/ffencoder.py
@@ -13,10 +13,8 @@ from .func_utils import mediainfo, convertBytes, convertTime, sendMessage, editM
 from .reporter import rep
 
 ffargs = {
+    'HDRIP': Var.FFCODE_HDRIP,
     '1080': Var.FFCODE_1080,
-    '720': Var.FFCODE_720,
-    '480': Var.FFCODE_480,
-    '360': Var.FFCODE_360,
 }
 
 class FFEncoder:
